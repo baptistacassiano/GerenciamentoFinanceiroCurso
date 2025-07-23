a317a60958a8b01bb618dfbb77dd36f406d444ff
baptistacassiano <tbaptista.cassiano@gmail.com>
2025-07-22 21:47:11 -0300

    Initial commit with project structure and database setup

---
+++ GerenciamentoFinanceiroCurso/Migrations/20250723001607_CriandoDataBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GerenciamentoFinanceiroCurso.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
