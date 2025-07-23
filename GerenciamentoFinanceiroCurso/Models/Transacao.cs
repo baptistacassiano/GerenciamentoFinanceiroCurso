3126b17331c9b748b0e7a94723c316f0807c09b6
baptistacassiano <tbaptista.cassiano@gmail.com>
2025-07-22 22:45:47 -0300

    Add models, update AppDbContext, and create migration for tables

---
+++ GerenciamentoFinanceiroCurso/Models/Transacao.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciamentoFinanceiroCurso.Models
+{
+    public class Transacao
+    {
+        public string TransacaoId { get; set; }
+        public string Nome { get; set; }
+    }
+}
